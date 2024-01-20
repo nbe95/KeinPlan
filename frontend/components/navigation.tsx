be4121a8fc49aecc84d76849c629a52897a3cb51
Niklas Bettgen <niklas@bettgen.de>
2024-01-20 13:40:21 +0100

    Add favicon

--- frontend/components/navigation.tsx
+++ frontend/components/navigation.tsx
@@ -1,11 +1,16 @@
 import { Navbar, Nav, Container } from "react-bootstrap";
 import { NextPage } from "next";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faChurch } from '@fortawesome/free-solid-svg-icons'
 
 const Navigation: NextPage = () => {
   return (
     <Navbar bg="dark" data-bs-theme="dark" expand="lg">
       <Container>
-        <Navbar.Brand href="/">KeinPlan</Navbar.Brand>
+        <Navbar.Brand href="/">
+          <FontAwesomeIcon icon={faChurch} className="me-2 text-warning" />
+          KeinPlan
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
--- frontend/components/page-wrapper.tsx
+++ frontend/components/page-wrapper.tsx
@@ -3,6 +3,8 @@ import Head from "next/head";
 import Navigation from "./navigation";
 import { PropsWithChildren } from "react";
 import { Container } from "react-bootstrap";
+import { config, dom } from "@fortawesome/fontawesome-svg-core";
+config.autoAddCss = false;
 
 type PageProps = {
   title: string;
@@ -13,7 +15,12 @@ const PageWrapper = (props: PropsWithChildren<PageProps>) => {
     <>
       <Head>
         <title>KeinPlan {props.title && ` | ${props.title}`}</title>
-      </Head>
+        <link rel="icon" href="/icon" sizes="any" />
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" href="/favicon.ico" />
+        <style>{dom.css()}</style>
+      </Head >
       <Navigation></Navigation>
       <Container fluid="sm">{props.children}</Container>
     </>
