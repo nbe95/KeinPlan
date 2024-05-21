2d5278c62ba05eedcc30a31bee52f4f3fc67b269
Niklas Bettgen <niklas@bettgen.de>
2024-05-21 19:16:03 +0200

    Use amazing date cards

--- frontend/components/time-sheets/date-card.tsx
+++ frontend/components/time-sheets/date-card.tsx
@@ -1,4 +1,5 @@
-import { Stack } from "react-bootstrap";
+import { Card, Stack } from "react-bootstrap";
+import strftime from "strftime";
 import { TimeSheetDate } from "./common";
 
 type DateCardProps = {
@@ -6,52 +7,43 @@ type DateCardProps = {
 };
 
 export const DateCard = (props: DateCardProps) => {
-  const getDateLabel = (date: Date): string => `${date.getDate().toString().padStart(2, "0")}.`;
-  const getMonthLabel = (date: Date): string => date.toLocaleString([], { month: "short" });
-
-  const getDurationLabel = (begin: Date, end: Date): string => {
-    const beginStr: string = begin.toLocaleTimeString([], {
-      hour12: false,
-      hour: "numeric",
-      minute: "2-digit",
-    });
-    const endStr: string = end.toLocaleTimeString([], {
-      hour12: false,
-      hour: "numeric",
-      minute: "2-digit",
-    });
-    return `${beginStr} - ${endStr}`;
-  };
-
+  const strftimeGer = strftime.localizeByIdentifier("de_DE");
   return (
-    <Stack direction="horizontal" gap={0} className="my-2 mx-0 d-flex align-items-stretch">
-      <div
-        className="bg-danger text-white text-center align-content-center border border-dark border-end-0 rounded-start"
-        style={{ width: "5em", minWidth: "5em", flex: "unset" }}
-      >
-        <p className="display-6 m-0">{getDateLabel(props.date.begin)}</p>
-        <p className="fs-6 fw-bold m-0">{getMonthLabel(props.date.begin)}</p>
+    <Card className="d-flex flex-row my-2 shadow-sm">
+      <div className="bg-light rounded-start border-end">
+        <div className="text-center m-3">
+          <p className="badge bg-danger rounded-0 my-0" style={{ textTransform: "uppercase" }}>
+            {strftimeGer("%a", props.date.begin)}
+          </p>
+          <p className="text-primary display-6 my-0" style={{ width: "3rem" }}>
+            {strftimeGer("%d", props.date.begin)}
+          </p>
+          <p className="badge text-muted border-top border-grey my-0">
+            {strftimeGer("%b", props.date.begin)}
+          </p>
+        </div>
       </div>
-      <div className="border border-dark rounded-end px-2 py-1">
-        <Stack direction="horizontal" gap={1} className="align-items-start">
-          <div className="me-auto">
-            <p className="my-0 fw-bold">{props.date.title}</p>
-            <p className="my-0">{props.date.role}</p>
-            <p className="my-0">
-              <small className="text-muted">{props.date.location}</small>
-            </p>
+      <div className="mx-3 text-nowrap overflow-auto">
+        <div className="py-3">
+          <span className="h5 fw-bold mt-0">{props.date.title}</span>
+          <div className="mb-1">
+            <Stack direction="horizontal" gap={3}>
+              <span>
+                {strftimeGer("%H:%M", props.date.begin)} - {strftimeGer("%H:%M", props.date.end)}
+              </span>
+              {props.date.breakBegin && props.date.breakEnd && (
+                <span className="text-muted">
+                  ({strftimeGer("%H:%M", props.date.breakBegin)} -{" "}
+                  {strftimeGer("%H:%M", props.date.breakEnd)} Pause)
+                </span>
+              )}
+            </Stack>
           </div>
-          <div className="text-end">
-            <p className="text-nowrap my-0">{getDurationLabel(props.date.begin, props.date.end)}</p>
-            {props.date.breakBegin && props.date.breakEnd && (
-              <p className="text-nowrap my-0 text-muted">
-                Pause {getDurationLabel(props.date.breakBegin, props.date.breakEnd)}
-              </p>
-            )}
-          </div>
-        </Stack>
+          <div className="">{props.date.role}</div>
+          <div className="small text-muted">{props.date.location}</div>
+        </div>
       </div>
-    </Stack>
+    </Card>
   );
 };
 
