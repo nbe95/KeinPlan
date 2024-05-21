9b0916ad9c33880df3e9f2075d92c6773752de9b
Niklas Bettgen <niklas@bettgen.de>
2024-05-21 21:37:19 +0200

    Fix title element

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,7 +1,7 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import type { AppProps } from "next/app";
-
+import "../public/style.css";
 const queryClient = new QueryClient();
 
 export default function App({ Component, pageProps }: AppProps) {
