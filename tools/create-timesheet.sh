acfeacfe751270857c5b76a66e224ecef49903e6
Niklas Bettgen <niklas@bettgen.de>
2024-05-28 15:37:17 +0200

    Apply struct changes to script

--- tools/create-timesheet.sh
+++ tools/create-timesheet.sh
@@ -16,13 +16,27 @@ date_to=$(week2date "$year" "$week" 7)
 
 out_file="Arbeitszeit_$year-$week.pdf"
 
-ics=$(curl -s -X GET -H "X-KaPlan-ICS: $KAPLAN_ICS" "$KEINPLAN_URL/api/v1/kaplan?from=$date_from&to=$date_to")
-json="$(echo "$ics" \
-    | jq --arg v "$EMPLOYER" '. + {employer: $v}' \
-    | jq --arg v "$EMPLOYEE" '. + {employee: $v}' \
-    | jq --arg v "$year" '. + {year: $v|tonumber}' \
-    | jq --arg v "$week" '. + {week: $v|tonumber}' \
-    | jq '.dates[].location |= sub("(?<addr>.*),.*"; "\(.addr)")')"	# strip second part of location after comma
-echo "$json"
+kaplan_dates=$(curl -s -X GET -H "X-KaPlan-ICS: $KAPLAN_ICS" "$KEINPLAN_URL/api/v1/kaplan?from=$date_from&to=$date_to")
+ts_json="$(jq \
+    --arg employer "$EMPLOYER" \
+    --arg employee "$EMPLOYEE" \
+    --arg year "$year" \
+    --arg week "$week" \
+    "{
+        employer: \$employer,
+        employee: \$employee,
+	year: \$year|tonumber,
+	week: \$week|tonumber,
+        dates: [.dates[] | {
+            title: .title,
+       	    role: .role,
+            location: .location_short,
+            time: {
+                begin: .begin,
+                end: .end
+            }
+        }]
+    }" <<< "$kaplan_dates")"
 
-curl -s -X POST -H "Content-type: application/json" --data "$json" "$KEINPLAN_URL/api/v1/time-sheet/weekly/pdf?nofooter" > "$out_file"
+echo "$ts_json"
+curl -s -X POST -H "Content-type: application/json" --data "$ts_json" "$KEINPLAN_URL/api/v1/time-sheet/weekly/pdf?nofooter" > "$out_file"
