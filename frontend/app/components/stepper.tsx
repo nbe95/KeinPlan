8936ab7405c0738fd9e6f2697ed651eccc1b65f2
Niklas Bettgen <niklas@bettgen.de>
2024-06-23 09:09:05 +0200

    Mark active step

--- frontend/app/components/stepper.tsx
+++ frontend/app/components/stepper.tsx
@@ -12,24 +12,27 @@ type StepperProps = {
 };
 
 export const Stepper = (props: StepperProps) => {
-  return (
-    <ol className="stepper py-3">
-      {props.steps.map((step, index) => {
-        const isActive = props.active >= step.key;
+  const renderStep = (step: Step, index: number) => {
+        const isPassed = props.active >= step.key;
+        const isActive = props.active == step.key;
         return (
-          <li className={isActive && "active"} key={index}>
+          <li className={isPassed && "passed"} key={index}>
             <div className="mx-auto text-muted fw-bold fs-6">
               <div
-                className={`rounded-circle p-2 border ${isActive ? "border-primary bg-primary text-white" : "border-secondary bg-light text-muted"}`}
+                className={`rounded-circle p-2 border ${isPassed ? "border-primary bg-primary text-white" : "border-secondary bg-light text-muted"}`}
                 style={{ width: "2.6rem", height: "2.6rem" }}
               >
                 {step.icon ? <FontAwesomeIcon icon={step.icon} /> : <>{index + 1}</>}
               </div>
             </div>
-            <p className={`my-2 ${isActive ? "text-dark" : "text-muted"}`}>{step.name}</p>
+            <p className={`my-2 ${isActive ? "text-primary fw-bold" : (isPassed ? "text-dark" : "text-muted")}`}>{step.name}</p>
           </li>
         );
-      })}{" "}
+  }
+
+  return (
+    <ol className="stepper py-3">
+      {props.steps.map((step, index) => renderStep(step, index))}
     </ol>
   );
 };
