db50a1bd70597a6bee54ece97259811a76fbcb7d
Niklas Bettgen <niklas@bettgen.de>
2024-08-21 11:16:41 +0200

    Visual makeover

--- frontend/app/components/disclaimer.tsx
+++ frontend/app/components/disclaimer.tsx
@@ -1,9 +1,12 @@
 "use client";
 
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
+import { faCookie, faHardDrive } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { unstable_noStore as noStore } from "next/cache";
 import Link from "next/link";
 import { useState } from "react";
-import { Button, Col, Modal, Row } from "react-bootstrap";
+import { Accordion, Button, Col, Modal, Row } from "react-bootstrap";
 import { ADMIN_MAIL, GITHUB_LINK } from "../utils/constants";
 import { createMailToLink } from "../utils/mail";
 import Container from "./layout/container";
@@ -21,50 +24,48 @@ const Disclaimer = () => {
       <Row gap={5}>
         <Col md={6}>
           <div className="mt-5">
-            <h2>Haftungsausschluss</h2>
+            <h2>Fragen? Unklarheiten?</h2>
             <p>
-              <q>KeinPlan</q> ist ein rein privates Projekt und steht in keinerlei Verbindung mit{" "}
-              KaPlan oder einer spezifischen Pfarrgemeinde.
-            </p>
-            <p>
-              Jegliche Nutzung, insbesondere bzgl. des Inhalts erstellter Stunden&shy;listen,
-              erfolgt rein auf eigene Verantwortung des Anwenders und <b>ohne jegliche Gewähr</b>.
-            </p>
-            <p>
-              Bitte beachte, dass auf dieser Webseite nur spezielle KaPlan-Arbeitsgruppen und/oder
-              -Server vom Administrator freigeschaltet sein könnten.
-            </p>
-          </div>
-        </Col>
-
-        <Col md={6}>
-          <div className="mt-5">
-            <h2>Datenschutz</h2>
-            <p>
-              Dieses Tool verarbeitet personenbezogene Daten.{" "}
-              <Link href="#" onClick={() => setShowPrivacy(true)}>
-                Lies hier
-              </Link>
-              , was genau das bedeutet. Mit der Erstellung einer Stundenliste stimmst du der
-              Verarbeitung deiner Daten zu diesem Zweck zu.
-            </p>
-          </div>
-
-          <div className="mt-5">
-            <h2>Fragen oder Unklarheiten?</h2>
-            <p>
-              Kommt vor. Melde dich beim{" "}
+              Kommt vor. Melde dich einfach beim{" "}
               <CondLink condition={!!ADMIN_MAIL} href={createMailToLink({ recipient: ADMIN_MAIL })}>
                 Admin deines Vertrauens
               </CondLink>
               .
             </p>
             <p>
-              Solltest du einen Fehler finden, melde bitte auch dies oder{" "}
+              Solltest du einen Fehler auf dieser Seite oder einer Stunden&shy;liste
+              fest&shy;stellen, melde dies bitte oder{" "}
               <CondLink condition={!!GITHUB_LINK} href={`${GITHUB_LINK}/issues`}>
                 erstelle ein Ticket
               </CondLink>{" "}
               auf GitHub.
+            </p>
+          </div>
+
+          <div className="mt-5">
+            <h2>Datenschutz</h2>
+            <p>
+              Dieses Tool{" "}
+              <Link href="#" onClick={() => setShowPrivacy(true)}>
+                verarbeitet personenbezogene Daten
+              </Link>
+              . Mit der Erstellung einer Stundenliste stimmst du der Datenverarbeitung zu diesem
+              Zweck zu.
+            </p>
+          </div>
+        </Col>
+
+        <Col md={6}>
+          <div className="mt-5">
+            <h2>Haftungsausschluss</h2>
+            <p>
+              <q>KeinPlan</q> ist ein rein privates Projekt und steht in keinerlei Verbindung zu{" "}
+              KaPlan oder einer bestimmten Pfarrgemeinde.
+            </p>
+            <p>
+              Jegliche Nutzung erfolgt rein auf eigene Verantwortung des Anwenders und ohne jegliche
+              Gewähr. Dies gilt insbesondere für den Inhalt und die formale Korrektheit erstellter
+              Stunden&shy;listen.
             </p>
           </div>
         </Col>
@@ -76,68 +77,86 @@ const Disclaimer = () => {
           }}
           size="lg"
           scrollable={true}
+          fullscreen="sm-down"
         >
           <Modal.Header closeButton>
             <Modal.Title>Datenverarbeitung</Modal.Title>
           </Modal.Header>
           <Modal.Body>
-            <div className="mb-4">
-              <h5>Grundlegendes</h5>
-              <p>
-                <em>KeinPlan</em> verarbeitet im Zuge der Erstellung personali&shy;sierter
-                Stunden&shy;listen sensible, personen&shy;bezogene Daten. Sowohl diese Daten als
-                auch die generierten Stundenlisten werden{" "}
-                <b>zu keinem Zeitpunkt dauerhaft gespeichert</b>.
-              </p>
-              <p>
-                Jegliche Eingaben werden nur ein einziges Mal pro Anfrage verarbeitet, um entweder
-                Termindaten vom KaPlan-Server abzufragen oder Stundenlisten daraus zu generieren.
-                Diese werden lediglich bei Bedarf generiert und nach erfolgtem Download nicht im
-                Speicher oder Dateisystem des Servers persistiert.
-              </p>
-            </div>
+            <p>
+              <em>KeinPlan</em> verarbeitet im Zuge der Erstellung personali&shy;sierter
+              Stunden&shy;listen sensible, personen&shy;bezogene Daten. Sowohl diese Daten als auch
+              die generierten Stundenlisten werden zu keinem Zeitpunkt dauerhaft gespeichert.
+            </p>
+            <p>
+              Grundsätzlich werden jegliche Eingaben nur ein einziges Mal pro Anfrage verarbeitet,
+              sei es um entweder Termindaten vom KaPlan-Server abzufragen oder Stundenlisten daraus
+              zu generieren. Diese werden ausschließlich auf Anfrage generiert. Nach erfolgtem
+              Download werden sie nicht im Speicher oder Dateisystem des Servers persistiert oder
+              archiviert.
+            </p>
 
-            <div className="mb-4">
-              <h5>Open source</h5>
-              <p>
-                Der gesamte Code von <em>KeinPlan</em> ist quelloffen. Er ist im Zuge größtmöglicher
-                Transparenz, insb. hinsichtlich Datenverarbeitung, jederzeit öffentlich einsehbar
-                und auf GitHub verfügbar.
-                <br />
-                <CondLink condition={!!GITHUB_LINK} href={GITHUB_LINK}>
-                  {GITHUB_LINK}
-                </CondLink>{" "}
-              </p>
-            </div>
+            <p className="mt-4">Lies hier im Detail nach, was mit deinen Daten passiert:</p>
 
-            <div className="mb-4">
-              <h5>Serverseitiges Caching</h5>
-              <p>
-                Um unnötig wiederholte Anfragen an den KaPlan-Server zu vermeiden, gibt es einen
-                Caching-Mechanismus. Dabei wird jeder Abonnement-String nach gewissen Prüfschritten
-                inkl. der damit verbundenen Termindaten <b>für kurze Zeit</b> im Backend
-                vorgehalten. Das passiert verschlüsselt als sog.{" "}
-                <Link href="https://de.wikipedia.org/wiki/Hashfunktion">Hash</Link>, sodass bereits
-                angefragte Datensätze direkt zugeordnet und unnötiger Traffic vermieden werden, aber
-                der jeweils zugrunde&shy;liegende Abonnement-String daraus nicht rekonstruiert
-                werden kann.
-              </p>
-              <p>
-                Die Daten&shy;integrität bleibt somit gewährleistet, denn selbst aus Server Logs
-                oder einem Speicher&shy;abbild können zu keinem Zeitpunkt sensible Daten ausgelesen
-                werden.
-              </p>
-            </div>
+            <Accordion>
+              <Accordion.Item eventKey="server-side-caching">
+                <Accordion.Header>
+                  <FontAwesomeIcon icon={faHardDrive} className="me-2" /> Serverseitiges Caching
+                </Accordion.Header>
+                <Accordion.Body>
+                  <p>
+                    Um unnötig wiederholte Anfragen an den KaPlan Upstream-Server zu vermeiden,
+                    implementiert das Backend einen Caching-Mechanismus. Dabei wird jeder
+                    Abonnement-String nach gewissen Prüfschritten inkl. der damit rückgemeldeten
+                    Termindaten kurzzeitig im Speicher vorgehalten. Das passiert verschlüsselt als
+                    sog. <Link href="https://de.wikipedia.org/wiki/Hashfunktion">Hash</Link>, sodass
+                    bereits angefragte Datensätze direkt zugeordnet und unnötiger Traffic vermieden
+                    werden, aber der jeweils zugrunde&shy;liegende Abonnement-String daraus nicht
+                    rekonstruiert werden kann.
+                  </p>
+                  <p>
+                    Die Daten&shy;integrität bleibt somit gewährleistet. Selbst aus Server-Logs oder
+                    einem Speicher&shy;abbild können zu keinem Zeitpunkt sensible Daten ausgelesen
+                    werden.
+                  </p>
+                </Accordion.Body>
+              </Accordion.Item>
 
-            <div className="mb-4">
-              <h5>Cookies</h5>
-              <p>
-                Für eine einfachere Nutzbarkeit des Tools bei der wiederholten Anfertigung von
-                Stundenlisten können die Werte aller Eingabe&shy;felder lokal als Cookie gespeichert
-                werden. Diese Daten verlassen die aktuelle Browser-Sitzung nicht und werden zu
-                keinem anderen Zweck ausgelesen oder anderweitig verarbeitet.
-              </p>
-            </div>
+              <Accordion.Item eventKey="cookies">
+                <Accordion.Header>
+                  <FontAwesomeIcon icon={faCookie} className="me-2" />
+                  Cookies
+                </Accordion.Header>
+                <Accordion.Body>
+                  <p>
+                    Für eine einfachere Nutzbarkeit des Tools bei wiederholter Anwendung können die
+                    eingegebenen Werte der Eingabe&shy;felder (Name, Arbeitgeber,
+                    KaPlan-Abonnement-String) gespeichert werden. Das passiert als Cookie im
+                    Browser. Die Daten verlassen die aktuelle Browser-Sitzung nicht und werden zu
+                    keinem anderen Zweck ausgelesen oder anderweitig verarbeitet.
+                  </p>
+                </Accordion.Body>
+              </Accordion.Item>
+
+              <Accordion.Item eventKey="open-source">
+                <Accordion.Header>
+                  <FontAwesomeIcon icon={faGithub} className="me-2" /> Open Source Software
+                </Accordion.Header>
+                <Accordion.Body>
+                  <p>
+                    Der gesamte Code von <em>KeinPlan</em> ist quelloffen. Er ist im Zuge
+                    größtmöglicher Transparenz jederzeit öffentlich auf GitHub einsehbar.
+                    <br />{" "}
+                    <CondLink condition={!!GITHUB_LINK} href={GITHUB_LINK}>
+                      {GITHUB_LINK}
+                    </CondLink>{" "}
+                    <p></p>
+                    Jeder mit technischem Interesse/Kenntnissen kann einen eigenen <em>KeinPlan</em>
+                    -Server bzw. -Dienst lokal betreiben, z.&nbsp;B. bequem im Docker Container.
+                  </p>
+                </Accordion.Body>
+              </Accordion.Item>
+            </Accordion>
           </Modal.Body>
           <Modal.Footer>
             <Button variant="primary" onClick={() => setShowPrivacy(false)}>
