2f48ab6aadb0cb02924ebf6f6b292ca4a8f7381f
Niklas Bettgen <niklas@bettgen.de>
2024-08-22 11:24:03 +0200

    Distinguish build/runtime vars

--- frontend/app/utils/constants.ts
+++ frontend/app/utils/constants.ts
@@ -1,9 +1,11 @@
 import { env } from "next-runtime-env";
 
-// Environment variables
-export const PROD: boolean = process.env.NODE_ENV == "production"; // fetched at build-time
-export const GITHUB_LINK: string | undefined = env("NEXT_PUBLIC_GITHUB_LINK");
-export const VERSION_FRONTEND: string | undefined = env("NEXT_PUBLIC_VERSION_FRONTEND");
+// Environment variables (available at build-time)
+export const PROD: boolean = process.env.NODE_ENV == "production";
+export const GITHUB_LINK: string | undefined = process.env.NEXT_PUBLIC_GITHUB_LINK;
+export const VERSION_FRONTEND: string | undefined = process.env.NEXT_PUBLIC_VERSION_FRONTEND;
+
+// Environment variables (fetched at runtime)
 export const ADMIN_MAIL: string | undefined = env("NEXT_PUBLIC_ADMIN_MAIL");
 export const TIME_SHEET_MAIL: string | undefined = env("NEXT_PUBLIC_TIME_SHEET_MAIL");
 export const KAPLAN_LINK: string | undefined = env("NEXT_PUBLIC_KAPLAN_LINK");
