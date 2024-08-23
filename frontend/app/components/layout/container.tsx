226c612a5ccb75c6a766cb99d98612a60b7fbb4e
Niklas Bettgen <niklas@bettgen.de>
2024-08-23 16:28:51 +0200

    Optimize y padding for mobile devices

--- frontend/app/components/layout/container.tsx
+++ frontend/app/components/layout/container.tsx
@@ -10,7 +10,7 @@ export const Container = (props: PropsWithChildren<ContainerProps>) => {
   return (
     <div className={props.className} id={props.id}>
       <Row className="mx-auto">
-        <Col lg={8} className="mx-auto px-4 px-md-5 py-4">
+        <Col lg={8} className="mx-auto px-4 px-md-5 py-3 py-sm-4">
           {props.children}
         </Col>
       </Row>
