4ac18bda34e63408313e80f9dfb9497af97794b9
Niklas Bettgen <niklas@bettgen.de>
2024-08-24 18:43:58 +0200

    Sanitize LaTeX input

---
+++ backend/src/time_sheets/templates/jinja_filters.py
@@ -0,0 +1,30 @@
+"""Module for custom Jinja filters."""
+
+from typing import Dict
+
+
+def sanitize_latex(inputStr: str) -> str:
+    """Sanitize input for use in LaTeX templates by escaping special chars."""
+    specialCharMap: Dict[str, str] = {
+        "'": r"\textquotesingle{}",
+        '"': r"\textquotedbl{}",
+        "`": r"\textasciigrave{}",
+        "^": r"\textasciicircum{}",
+        "~": r"\textasciitilde{}",
+        "<": r"\textless{}",
+        ">": r"\textgreater{}",
+        "|": r"\textbar{}",
+        "\\": r"\textbackslash{}",
+        "{": r"\{",
+        "}": r"\}",
+        "$": r"\$",
+        "&": r"\&",
+        "#": r"\#",
+        "_": r"\_",
+        "%": r"\%",
+    }
+    # Iterate over each character to avoid replacing control commands more than once
+    result: str = ""
+    for char in inputStr:
+        result += specialCharMap.get(char, char)
+    return result
