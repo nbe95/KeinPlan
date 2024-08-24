ddb617959e0351f162fe02ef330807fc1486f1fa
Niklas Bettgen <niklas@bettgen.de>
2024-08-24 09:42:37 +0200

    Set white theme color

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
             <link rel="manifest" href="/site.webmanifest" />
             <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
             <meta name="msapplication-TileColor" content="#2d89ef" />
-            <meta name="theme-color" content="#eff" />
+            <meta name="theme-color" content="#ffffff" />
             <PublicEnvScript />
           </head>
           <body>
