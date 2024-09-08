01c21a556f2fba80020cddcc8273ca85aa22c731
Niklas Bettgen <niklas@bettgen.de>
2024-09-08 19:16:02 +0200

    Make responsive

--- frontend/app/components/time-sheet/date-filter.tsx
+++ frontend/app/components/time-sheet/date-filter.tsx
@@ -1,19 +1,19 @@
-import { Badge, Nav } from "react-bootstrap";
+import { Badge, Dropdown, Nav } from "react-bootstrap";
 import strftime from "strftime";
 import { addDaysToDate, isSameDay } from "../../utils/dates";
 
-type DateFilterTabProps = {
+type FilterProps = {
   eventKey: string;
   title: string;
   disabled: boolean;
   filterValue?: Date;
   badge?: number;
-
+  className?: string;
   activeFilter?: Date;
   setActiveFilter: (date?: Date) => void;
 };
 
-const FilterTab = (props: DateFilterTabProps) => {
+const FilterTab = (props: FilterProps) => {
   const isActive =
     props.activeFilter == props.filterValue ||
     (props.activeFilter !== undefined &&
@@ -23,10 +23,11 @@ const FilterTab = (props: DateFilterTabProps) => {
   return (
     <Nav.Item>
       <Nav.Link
+        active={isActive}
         eventKey={props.eventKey}
         onClick={() => props.setActiveFilter(props.filterValue)}
         disabled={props.disabled}
-        className={isActive ? "text-primary" : isDisabled ? "text-muted" : "text-dark"}
+        className={`${props.className} ${isActive ? "text-primary" : isDisabled ? "text-muted" : "text-dark"}`}
       >
         {props.title}
         {!isDisabled && (
@@ -36,6 +37,29 @@ const FilterTab = (props: DateFilterTabProps) => {
         )}
       </Nav.Link>
     </Nav.Item>
+  );
+};
+
+const FilterItem = (props: FilterProps) => {
+  const isActive =
+    props.activeFilter == props.filterValue ||
+    (props.activeFilter !== undefined &&
+      props.filterValue !== undefined &&
+      isSameDay(props.activeFilter, props.filterValue));
+  const isDisabled = props.badge === undefined;
+  return (
+    <Dropdown.Item
+      onClick={() => props.setActiveFilter(props.filterValue)}
+      disabled={props.disabled}
+      className={`${props.className} ${isActive ? "text-primary" : isDisabled ? "text-muted" : "text-dark"}`}
+    >
+      {props.title}
+      {!isDisabled && (
+        <Badge bg={isActive ? "primary" : "secondary"} pill className="ms-2 small">
+          {props.badge}
+        </Badge>
+      )}
+    </Dropdown.Item>
   );
 };
 
@@ -75,9 +99,43 @@ export const WeekFilter = (props: WeekFilterProps) => {
               badge={occurrences || undefined}
               activeFilter={props.activeFilter}
               setActiveFilter={() => props.setActiveFilter(day)}
+              className="d-none d-xl-block"
             />
           );
         })}
+
+      <Dropdown as={Nav.Item} className="d-block d-xl-none">
+        <Dropdown.Toggle
+          as={Nav.Link}
+          active={props.activeFilter !== undefined}
+          className={props.activeFilter !== undefined ? "text-primary" : "text-dark"}
+        >
+          Filter…
+        </Dropdown.Toggle>
+
+        <Dropdown.Menu>
+          {Array(7)
+            .fill(0)
+            .map((_, dayOffset) => {
+              const day: Date = addDaysToDate(props.baseDate, dayOffset);
+              const occurrences: number = props.dateList.filter((date) =>
+                isSameDay(date, day),
+              ).length;
+              return (
+                <FilterItem
+                  key={`${keyPrefix}-${dayOffset}`}
+                  eventKey={`${keyPrefix}-${dayOffset}`}
+                  title={strftimeGer("%A", day)}
+                  disabled={occurrences == 0}
+                  filterValue={day}
+                  badge={occurrences || undefined}
+                  activeFilter={props.activeFilter}
+                  setActiveFilter={() => props.setActiveFilter(day)}
+                />
+              );
+            })}
+        </Dropdown.Menu>
+      </Dropdown>
     </Nav>
   );
 };
