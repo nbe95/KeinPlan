31e6b9ae4c44b8f47c9f9c7dd31ea094c302cc89
Niklas Bettgen <niklas@bettgen.de>
2025-01-07 19:40:15 +0100

    Add explaining comment

--- frontend/app/components/time-sheet/steps/result.tsx
+++ frontend/app/components/time-sheet/steps/result.tsx
@@ -149,12 +149,14 @@ const ResultStep = (props: ResultProps) => {
               <p>Überprüfe vorher nochmal alles auf Richtigkeit.</p>
               {TIME_SHEET_MAIL && (
                 <Obfuscate
-                  // @ts-expect-error
-                  id="open-mail-template"
-                  className="btn btn-primary"
                   email={TIME_SHEET_MAIL}
                   headers={mailHeaders}
                   obfuscateChildren={false}
+                  // Obfuscate allows passing arbitrary props, but does not define them in its
+                  // interface, which causes a type error on compiling
+                  // @ts-expect-error
+                  id="open-mail-template"
+                  className="btn btn-primary"
                 >
                   <FontAwesomeIcon icon={faEnvelopeOpenText} className="me-2" />
                   Mail-Vorlage öffnen
